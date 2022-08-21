ab84c454fdea080b35a1f8438639d306cba85b4a
natalia sizikova <mogilnaya2012@mail.ru>
2022-08-21 13:44:29 +0700

    Решение 23 задачи

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1,8,27
+//5 -> 1,8,27,64,125
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while(index<= n)
+{
+    Console.Write($"{index * index * index} ");
+    index ++;
+}
+Console.WriteLine();
